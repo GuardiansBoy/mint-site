9552cdd50c60f01cdb8791636eff60fe937a8ed8
GuardiansBoy <Guardiansgame11@gmail.com>
2022-10-30 03:01:02 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const btn = document.getElementById('btn')
+const nav = document.getElementById('nav')
+
+// btn.addEventListener('click', () => {
+//     nav.classList.toggle('active');
+//     btn.classList.toggle('active')
+// });
+
+const countdown = () => {
+    const countDate = new Date("Nov 3, 2022 00:00:00").getTime();
+    console.log(countDate);
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    //Calculate
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    document.querySelector(".day").innerText = textDay;
+    document.querySelector(".hour").innerText = textHour;
+    document.querySelector(".minute").innerText = textMinute;
+    document.querySelector(".second").innerText = textSecond;
+}
+
+setInterval(countdown, 1000);
